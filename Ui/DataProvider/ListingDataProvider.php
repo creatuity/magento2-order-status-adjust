43dc1034302774025a5919d9c2ec7f5040899676
Tomasz Szmyt <tszmyt@creatuity.com>
2023-02-14 14:38:10 +0100

    Grid works

--- Ui/DataProvider/ListingDataProvider.php
+++ Ui/DataProvider/ListingDataProvider.php
@@ -4,18 +4,49 @@ declare(strict_types=1);
 
 namespace Creatuity\OrderStatusAdjust\Ui\DataProvider;
 
-use Magento\Framework\Api\Filter;
+use Creatuity\OrderStatusAdjust\Model\ResourceModel\Rule\CollectionFactory;
 use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\ReportingInterface;
 use Magento\Framework\Api\Search\SearchCriteriaBuilder;
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\AuthorizationInterface;
-use Magento\Framework\View\Element\UiComponent\DataProvider\Reporting;
 
 /**
  * todo: see how Cms listing data providers are written, adjust it to work (search criteria etc)
  */
 class ListingDataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
 {
+    public function __construct(
+        private readonly CollectionFactory $ruleCollectionFactory,
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        ReportingInterface $reporting,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        RequestInterface $request,
+        FilterBuilder $filterBuilder,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $reporting,
+            $searchCriteriaBuilder,
+            $request,
+            $filterBuilder,
+            $meta,
+            $data
+        );
+    }
 
+    public function getData(): array
+    {
+        $collection = $this->ruleCollectionFactory->create();
+        $rules = $collection->toArray();
+
+        return [
+            'items' => $rules['items']
+        ];
+    }
 }
