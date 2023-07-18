c94f7b47237f565df32bd3f1ac6a92750445f038
Tomasz Szmyt <tszmyt@creatuity.com>
2023-07-18 11:29:41 +0200

    Unit Tests - GetStateByStatusTest and ValidationHandlerTest

--- Api/Data/RuleInterface.php
+++ Api/Data/RuleInterface.php
@@ -2,7 +2,19 @@
 
 namespace Creatuity\OrderStatusAdjust\Api\Data;
 
+use Creatuity\OrderStatusAdjust\Model\Condition\Combine;
+use Magento\Rule\Model\Action\Collection;
+
 interface RuleInterface
 {
     public const IS_ACTIVE = 'is_active';
+
+    public function getConditionsInstance(): Combine;
+    public function getActionsInstance(): Collection;
+    public function getConditionsFieldSetId(string $formName = ''): string;
+
+    public function getFromDate(): ?string;
+    public function getToDate(): ?string;
+
+    public function validateData(\Magento\Framework\DataObject $dataObject);
 }
